5952fc8322c00d75e64e8f6df1c46805de515912
Sakibur Rahman <sakib.cse.333@gmail.com>
2026-09-04 11:36:46 +0600

    Add ViewGamesScreen.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,8 @@
-import ThemeToggle from "@/components/ThemeToggle";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import ViewGamesScreen from "@/modules/games/view-games/screen/ViewGamesScreen";
 
 const GamesRoute = () => {
     return (
-        <SafeAreaView edges={["top", "left", "right"]} className="flex-1 px-4 bg-background">
-        <View>
-            <Text className="text-2xl font-bold text-primary">
-                Hazaro
-            </Text>
-
-            <Text className="mt-1 text-muted-foreground">
-                Your recent games
-            </Text>
-
-            <View className="mt-6 rounded-2xl border border-border bg-surface p-4">
-                <Text className="text-lg font-semibold text-foreground">
-                    Knight Game
-                </Text>
-
-                <Text className="mt-1 text-muted-foreground">
-                    4 players
-                </Text>
-            </View>
-            <ThemeToggle />
-        </View>
-        </SafeAreaView>
+        <ViewGamesScreen />
     );
 };
 
