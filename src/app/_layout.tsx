b851776eb08d7177b59c137cbe17bb9cb02c0fce
Sakibur Rahman <sakib.cse.333@gmail.com>
2026-09-16 22:50:47 +0600

    Update status bar bg color.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,23 @@
 import { AppProvider } from '@/components/AppProvider';
 import { Stack } from 'expo-router';
 import { useTranslation } from 'react-i18next';
+import { StatusBar, useColorScheme } from 'react-native';
 import "../global.css";
 
 const RootLayout = () => {
   const { t } = useTranslation();
+  const colorScheme = useColorScheme();
+
+  const isDark = colorScheme === "dark";
+
   return (
     <AppProvider>
+      <StatusBar
+        hidden={false}
+        backgroundColor={isDark ? "#020617" : "#FFFFFF"}
+        barStyle={isDark ? 'light-content' : 'dark-content'}
+      />
+
       <Stack>
         <Stack.Screen
           name="(tabs)"
